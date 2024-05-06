d1a88e9e21fe347b9c13daf67495f4bb59103ba5
Aziz <aziko.mz@gmail.com>
2024-05-06 18:00:25 +0300

    MVP

--- ResultComparer/results/models.py
+++ ResultComparer/results/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Apps(models.Model):
+    name = models.CharField(max_length=40)
+    about = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.name
--- ResultComparer/results/views.py
+++ ResultComparer/results/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Apps
 
-# Create your views here.
+def index(request):
+    # cards = {'available_cards': available_cards.keys()}
+    cards = {'available_apps': Apps.objects.all()}
+    return render(request, 'results/index.html', cards)
+
+def detail(request, app_id):
+    # card_detail = available_cards.get(card_id)
+    app_detail = {"app_detail": Apps.objects.get(name=app_id),
+                   'available_apps': Apps.objects.all()}
+    # return HttpResponse(f'This is {card_detail} card ')
+    return render(request, 'results/detail.html', app_detail)
+
+
+def show_available_options_in_main():
+    return HttpResponse("Hello from django")
